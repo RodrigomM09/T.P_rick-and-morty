0ddf5b7f6a06a78b2d69efa73d86dd4f1196ce79
rodrigomM09 <rodrimmanu_2020_@hotmail.com>
2023-08-31 23:00:59 -0300

    Api_rick and morty

--- Api_rick and morty/script.js
+++ Api_rick and morty/script.js
@@ -1,5 +1,5 @@
 
-const listaPersonajes = JSON.parse(localStorage.getItem("personajes")) || []
+let listaPersonajes = JSON.parse(localStorage.getItem("personajes")) || []
 
 const buscarPersonaje = async (id) =>{
     const dataPersonaje = await fetch(`https://rickandmortyapi.com/api/character/${id}/`)
@@ -17,6 +17,7 @@ const armarCard = (personaje) =>{
                         <h5 class="card-title">${personaje.name}</h5><br>
                         <p class="card-text"><strong>Status:</strong> ${personaje.status}</p>
                         <p class="card-text"><strong>Specie:</strong> ${personaje.species}</p>
+                        <button class="btn-close" onclick="eliminarPersonaje(${personaje.id})"></button>
                     </div>
             </div> `
 }
@@ -24,13 +25,23 @@ const armarCard = (personaje) =>{
 const agregarDatosCard = async () =>{
     const input = document.querySelector("input#input-personaje")
     const conteTarjeta = document.querySelector(".tarjeta")
-    const personajeEncontrado = await buscarPersonaje (input.value)
-    const cardlista = armarCard (personajeEncontrado)
-    conteTarjeta.innerHTML += cardlista
+    const id_maximos = 826
+    const seleccion_maxima = parseInt(input.value)
+    const personajeExistente = listaPersonajes.some(personaje => personaje.id === seleccion_maxima)
+
+    if (seleccion_maxima <= id_maximos && !personajeExistente) {
+        const personajeEncontrado = await buscarPersonaje(input.value)
+        const cardlista = armarCard(personajeEncontrado)
+        conteTarjeta.innerHTML += cardlista
+    } else if (seleccion_maxima > id_maximos) {
+        alert("Seleccione un numero menor o igual a 826")
+        } else {
+        alert("Ese personaje ya fue agregado")
+    }
 }
 
 const cargarPagina = () =>{
-    const conteTarjeta = documet.querySelector(".tarjeta")
+    const conteTarjeta = document.querySelector(".tarjeta")
     conteTarjeta.innerHTML = ""
     listaPersonajes.forEach(personaje =>{
         const cartaArmada = armarCard(personaje)
@@ -38,4 +49,25 @@ const cargarPagina = () =>{
     })
 }
 
+cargarPagina()
+
+
+document.addEventListener("DOMcontenidoCarga", function () {
+    const botonBuscar = document.getElementById("btn-buscar")
+    botonBuscar.addEventListener("click", agregarDatosCard)
+    const botonBorrarTodos = document.getElementById("btn-borrar-todos");
+    botonBorrarTodos.addEventListener("onclick", borrarTodosLosPersonajes);
+})
+function borrarTodosLosPersonajes() {
+    localStorage.removeItem("personajes")
+    const conteTarjeta = document.querySelector(".tarjeta")
+    conteTarjeta.innerHTML = ""
+    
+}
+
+function eliminarPersonaje(id) {
+    listaPersonajes = listaPersonajes.filter(personaje => personaje.id !== id)
+    localStorage.setItem("personajes", JSON.stringify(listaPersonajes))
+    cargarPagina()
+}
 
